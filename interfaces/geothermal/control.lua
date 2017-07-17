5904be7430409f06fc591f47976392813d64483e
nucleargen <nucleargen@gmail.com>
2017-07-17 23:32:04 +0700

    visualized heatmap on ingame map. done.

--- interfaces/geothermal/control.lua
+++ interfaces/geothermal/control.lua
@@ -1,6 +1,6 @@
 local chunk_size = 32
 local seed
-local gensize = 8192 -- 256 chunks
+local gensize = 256 -- 8 chunks
 local base_temp = 25
 local max_temp = 500
 local min_temp = 15
@@ -11,29 +11,29 @@ Square_Diamond_Gen.cache = {}
 Square_Diamond_Gen.cache_locked = false
 Square_Diamond_Gen.chunks_generated = 0
 
-Square_Diamond_Gen.get_value = function(x,y,val)
+Square_Diamond_Gen.get_value = function(x,y,val,region)
 	local index = x .. '_' .. y
-	-- log("gv: requested "..x..","..y)
+	-- if fcm_debug then log("gv: requested "..x..","..y) end
 	if val ~= nil then
 		Square_Diamond_Gen.cache[index] = math.max(min_temp,math.min(max_temp,val))
-	elseif x <= -gensize/2 or y <= -gensize/2 or x >= gensize/2 - 1 or y >= gensize/2 then
+	-- divide whole map into square regions with size = gensize and centered in map center
+	-- if fcm_debug then if x > -128 and x < 128 and y > -128 and y < 128 then log("gen modulo for "..x..","..y..": "..x % gensize * x / math.abs(x)..","..y % gensize * y / math.abs(y)..";"..gensize/2) end end
+	elseif x <= (region.x-1/2)*gensize or y <= (region.y-1/2)*gensize or x >= (region.x+1/2)*gensize or y >= (region.y+1/2)*gensize then
 		Square_Diamond_Gen.cache[index] = math.max(min_temp,math.min(max_temp,base_temp))
-	else
-		if Square_Diamond_Gen.cache[index] == nil then
-			local base = 1
-			-- recalc coords to basis
-			lx = x % gensize + gensize/2
-			ly = y % gensize + gensize/2
-			
-			while bit32.band(lx,base) == 0 and bit32.band(ly,base) == 0 do
-				-- log("gv: lshift "..base..","..bit32.band(lx,base)..","..bit32.band(ly,base))
-				base = bit32.lshift(base,1)
-			end
-			if  bit32.band(lx,base) ~= 0 and bit32.band(ly,base) ~= 0 then
-				Square_Diamond_Gen.square_step(x,y,base)
-			else
-				Square_Diamond_Gen.diamond_step(x,y,base)
-			end
+	elseif Square_Diamond_Gen.cache[index] == nil then
+		local base = 1
+		-- recalc coords to basis
+		lx = x % gensize + gensize/2
+		ly = y % gensize + gensize/2
+
+		while bit32.band(lx,base) == 0 and bit32.band(ly,base) == 0 do
+			-- if fcm_debug then log("gv: lshift "..base..","..bit32.band(lx,base)..","..bit32.band(ly,base)) end
+			base = bit32.lshift(base,1)
+		end
+		if  bit32.band(lx,base) ~= 0 and bit32.band(ly,base) ~= 0 then
+			Square_Diamond_Gen.square_step(x,y,base,region)
+		else
+			Square_Diamond_Gen.diamond_step(x,y,base,region)
 		end
 	end
 	return Square_Diamond_Gen.cache[index]
@@ -43,26 +43,26 @@ Square_Diamond_Gen.displace = function(val,block_size,x,y)
 	return math.floor(val + (Square_Diamond_Gen.rand_from_pair(x,y,seed) - 0.5) * (max_temp - min_temp) * block_size * 2 / gensize * roughness);
 end
 
-Square_Diamond_Gen.square_step = function(x,y,block_size)
-	--log("sq step:"..x..","..y)
+Square_Diamond_Gen.square_step = function(x,y,block_size,region)
+	-- if fcm_debug then log("sq step:"..x..","..y) end
 	if Square_Diamond_Gen.cache[x .. '_' .. y] == nil then
 		Square_Diamond_Gen.get_value(x,y,Square_Diamond_Gen.displace((
-			Square_Diamond_Gen.get_value(x-block_size,y-block_size) +
-			Square_Diamond_Gen.get_value(x+block_size,y-block_size) +
-			Square_Diamond_Gen.get_value(x-block_size,y+block_size) +
-			Square_Diamond_Gen.get_value(x+block_size,y+block_size)
-		)/4,block_size,x,y))
+			Square_Diamond_Gen.get_value(x-block_size,y-block_size,nil,region) +
+			Square_Diamond_Gen.get_value(x+block_size,y-block_size,nil,region) +
+			Square_Diamond_Gen.get_value(x-block_size,y+block_size,nil,region) +
+			Square_Diamond_Gen.get_value(x+block_size,y+block_size,nil,region)
+		)/4,block_size,x,y),region)
 	end
 end
-Square_Diamond_Gen.diamond_step = function(x,y,block_size)
-	--log("dia step:"..x..","..y)
+Square_Diamond_Gen.diamond_step = function(x,y,block_size,region)
+	-- if fcm_debug then log("dia step:"..x..","..y) end
 	if Square_Diamond_Gen.cache[x .. '_' .. y] == nil then
 		Square_Diamond_Gen.get_value(x,y,Square_Diamond_Gen.displace((
-			Square_Diamond_Gen.get_value(x-block_size,y) +
-			Square_Diamond_Gen.get_value(x+block_size,y) +
-			Square_Diamond_Gen.get_value(x,y-block_size) +
-			Square_Diamond_Gen.get_value(x,y+block_size)
-		)/4,block_size,x,y))
+			Square_Diamond_Gen.get_value(x-block_size,y,nil,region) +
+			Square_Diamond_Gen.get_value(x+block_size,y,nil,region) +
+			Square_Diamond_Gen.get_value(x,y-block_size,nil,region) +
+			Square_Diamond_Gen.get_value(x,y+block_size,nil,region)
+		)/4,block_size,x,y),region)
 	end
 end
 
@@ -97,12 +97,19 @@ Square_Diamond_Gen.process_chunk = function(event)
 	local maxy = event.area.right_bottom.y
 	-- x = minx
 	-- y = miny
+	local region = {
+		x	=	math.floor((minx + gensize/2)/gensize),
+		y	=	math.floor((miny + gensize/2)/gensize),
+	}
+	--if fcm_debug then 
+	log("geothermal region: "..region.x..","..region.y) 
+	--end
 	-- iterate left to right
 	for x = minx, maxx do
 		-- iterate up to down
 		for y = miny, maxy do
-			local value = Square_Diamond_Gen.get_value(x,y)
-			log("x,y:"..x..","..y..":value:"..value)
+			local value = Square_Diamond_Gen.get_value(x,y,nil,region)
+			if fcm_debug then log("x,y:"..x..","..y..":value:"..value) end
 			local color_index = math.floor(10 * (1 - (value - min_temp) / (max_temp - min_temp))) + 1
 			local overlay = event.surface.create_entity{
 				name = "gt_overlay_"..color_index,
